3818ccc4c41c80c565fa4bacf47458d874f031a4
Justin Hohing <jnh0627@yahoo.com>
2021-03-12 16:10:04 -0500

    Added viewAllEmployees and viewByDepartment functions

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const promptMessages = {
     viewByDepartment: "View All Employees By Department",
     viewManager: "View All Employees By Manager",
     addEmployee: "Add An Employee",
-    removeEmplyee: "Remove An Employee",
+    removeEmployee: "Remove An Employee",
     updatedRole: "Update Employee Role",
     updateEmployeeManager: "Update Employee Manager",
     viewAllRoles: "View All Roles",
@@ -26,3 +26,78 @@ connection.connect(err => {
     if(err) throw err;
     prompt();
 });
+
+function prompt(){
+    inquirer.prompt({
+        name: "activity",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            promptMessages.viewAllEmployees,
+            promptMessages.viewByDepartment,
+            promptMessages,viewManager,
+            promptMessages.viewAllRoles,
+            promptMessages.addEmployee,
+            promptMessages.removeEmployee,
+            promptMessages.updatedRole,
+            promptMessages.updateEmployeeManager,
+            promptMessages.exit
+        ]
+    })
+    .then(answer => {
+        switch(answer.activity){
+            case promptMessages.viewAllEmployees:
+                viewAllEmployees();
+                break;
+            case promptMessages.viewByDepartment:
+                viewByDepartment();
+                break;
+            case promptMessages.viewManager:
+                viewManager();
+                break
+            case promptMessages.viewAllRoles:
+                viewAllRoles();
+                break;
+            case promptMessages.addEmployee:
+                addEmployee();
+                break;
+            case promptMessages.removeEmployee:
+                removeEmployee();
+                break;
+            case promptMessages.updatedRole:
+                updatedRole();
+                break;
+            case promptMessages.exit:
+                connection.end();
+                break;
+        }
+    });
+};
+
+function viewAllEmployees (){
+    const query = `SELECT emp.id, emp.first_name, emp.last_name, role.title, dept.name AS department, role.salary, CONCAT(mngr.first_name, " ", mngr.last_name) AS manager
+    FROM employee emp
+    LEFT JOIN employee mngr ON mngr.id = emp.manager_id
+    INNER JOIN role ON role.id = emp.role_id
+    INNER JOIN department dept ON dept.id = role.department_id
+    ORDER BY emp.id`
+
+    connection.query(query, (err,results) => {
+        if(err) throw err;
+        console.table(results);
+        prompt();
+    });
+};
+
+function viewByDepartment() {
+    const query = `SELECT dept.name AS department, role.title, emp.id, emp.first_name, emp.last_name
+    FROM employee emp
+    LEFT JOIN role ON role.id = emp.role_id
+    LEFT JOIN department dept ON dept.id = role.department_id
+    ORDER BY emp.id`
+
+    connection.query(query, (err,results) => {
+        if(err) throw err;
+        console.table(results);
+    })
+};
